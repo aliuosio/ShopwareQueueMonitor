0d750c89b71d5a7cd44b3e6b504f1569b3b5a26c
Osiozekhai Aliu <oaliu@hotmail.com>
2024-11-27 17:07:48 +0100

    removed obsolete config object

--- src/Service/NotifierService.php
+++ src/Service/NotifierService.php
@@ -4,18 +4,13 @@ declare(strict_types=1);
 
 namespace QueueMonitor\Service;
 
-use Shopware\Core\System\SystemConfig\SystemConfigService;
-
 class NotifierService
 {
-    private SystemConfigService $configService;
-    private iterable $notifiers;
 
-    public function __construct(SystemConfigService $configService, iterable $notifiers)
-    {
-        $this->configService = $configService;
-        $this->notifiers = $notifiers;
-    }
+    public function __construct(
+        readonly private iterable $notifiers
+    )
+    {}
 
     public function run(string $message): void
     {
