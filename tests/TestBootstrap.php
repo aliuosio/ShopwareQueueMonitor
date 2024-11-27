10edef8bed40ebc03b2083701d0bfa285218bff9
Osiozekhai Aliu <oaliu@hotmail.com>
2024-11-27 04:39:44 +0100

    initial commit

---
+++ tests/TestBootstrap.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use Shopware\Core\TestBootstrapper;
+
+$loader = (new TestBootstrapper())
+    ->addCallingPlugin()
+    ->addActivePlugins('QueueMonitor')
+    ->setForceInstallPlugins(true)
+    ->bootstrap()
+    ->getClassLoader();
+
+$loader->addPsr4('QueueMonitor\\Tests\\', __DIR__);
