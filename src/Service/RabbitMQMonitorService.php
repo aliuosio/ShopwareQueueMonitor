f96b8a9f7f6189c39da7de9b5e7968fc9cb50baa
Osiozekhai Aliu <oaliu@hotmail.com>
2024-11-27 20:55:53 +0100

    refactored

--- src/Service/RabbitMQMonitorService.php
+++ src/Service/RabbitMQMonitorService.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace QueueMonitor\Service;
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-
 class RabbitMQMonitorService
 {
     /**
@@ -13,29 +11,9 @@ class RabbitMQMonitorService
      */
     public function checkStatus(): array
     {
-        $result = [
+        return [
             'status' => 'unhealthy',
             'error' => 'not good',
         ];
-
-        /**
-        try {
-            $connection = new AMQPStreamConnection('rabbitmq_host', 5672, 'user', 'password');
-            $channel = $connection->channel();
-            $queueStatus = $channel->queue_declare('queue_name', true);
-
-            $result = [
-                'status' => 'healthy',
-                'message_count' => $queueStatus[1],
-            ];
-        } catch (\Exception $e) {
-            $result = [
-                'status' => 'unhealthy',
-                'error' => $e->getMessage(),
-            ];
-        }
-         */
-
-        return $result;
     }
 }
