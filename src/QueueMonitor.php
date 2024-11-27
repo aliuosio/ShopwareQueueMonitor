07b9c974a44c07c675ba0ef91138bc65dde8af1d
Osiozekhai Aliu <oaliu@hotmail.com>
2024-11-27 07:26:29 +0100

    refactored

--- src/QueueMonitor.php
+++ src/QueueMonitor.php
@@ -8,7 +8,6 @@ use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
-use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 
 class QueueMonitor extends Plugin
@@ -16,17 +15,6 @@ class QueueMonitor extends Plugin
     public function install(InstallContext $installContext): void
     {
         // Do stuff such as creating a new payment method
-    }
-
-    public function uninstall(UninstallContext $uninstallContext): void
-    {
-        parent::uninstall($uninstallContext);
-
-        if ($uninstallContext->keepUserData()) {
-            return;
-        }
-
-        // Remove or deactivate the data created by the plugin
     }
 
     public function activate(ActivateContext $activateContext): void
