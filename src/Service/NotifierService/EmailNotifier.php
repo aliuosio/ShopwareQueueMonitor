530988ec54e28f18a8142931d1a1fff7d935ff0d
Osiozekhai Aliu <oaliu@hotmail.com>
2024-11-27 12:16:52 +0100

    WIP

--- src/Service/NotifierService/EmailNotifier.php
+++ src/Service/NotifierService/EmailNotifier.php
@@ -5,21 +5,39 @@ declare(strict_types=1);
 namespace QueueMonitor\Service\NotifierService;
 
 use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class EmailNotifier
 {
     public function __construct(
         readonly private SystemConfigService $configService,
+        readonly private MailerInterface $mailer,
     )
     {
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     *
+     * @todo refactor
+     */
     public function notify(string $message): void
     {
         if ($this->hasEmailNotify()) {
             if (!$this->getRecipient()) {
                 throw new \RuntimeException('Email recipient is not configured.');
             }
+
+            $email = (new Email())
+                ->from('no-reply@example.com')
+                ->to($this->getRecipient())
+                ->subject('Queue went bad')
+                ->text($message)
+                ->html('<p>' . nl2br($message) . '</p>');
+
+            $this->mailer->send($email);
         }
     }
 
