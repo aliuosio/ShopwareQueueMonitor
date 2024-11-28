b8fca2249211d76d0fd55902ceb5a9931295e576
Osiozekhai Aliu <oaliu@hotmail.com>
2024-11-29 00:06:46 +0100

    fallback to PhpAmqpLib\Connection\AMQPStreamConnection for connection test

--- src/Service/RabbitMQMonitorService.php
+++ src/Service/RabbitMQMonitorService.php
@@ -4,16 +4,39 @@ declare(strict_types=1);
 
 namespace QueueMonitor\Service;
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
 class RabbitMQMonitorService
 {
-    /**
-     * @todo use symfony messenger qeue for RabbitMQ
-     */
     public function checkStatus(): array
     {
-        return [
-            'status' => 'unhealthy',
-            'error' => 'not good',
-        ];
+        try {
+            $result = [
+                'status' => 'healthy',
+                'message_count' => $this->canConnect()[1],
+            ];
+        } catch (\Exception $e) {
+            $result = [
+                'status' => 'unhealthy',
+                'error' => $e->getMessage(),
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function canConnect(): ?array
+    {
+        $connection = new AMQPStreamConnection(
+            getenv('RABBITMQ_HOST'),
+            getenv('RABBITMQ_PORT'),
+            getenv('RABBITMQ_USER'),
+            getenv('RABBITMQ_PASSWORD')
+        );
+
+        return $connection->channel()->queue_declare('queue_monitor', true);
     }
 }
